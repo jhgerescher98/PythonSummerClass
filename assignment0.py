d61b9fde98ffac31bb17412f5000282b5d8b33d2
josephgerescher98 <jhgerescher98@gmail.com>
2020-06-07 12:13:17 -0500

    Create assignment0.py

---
+++ assignment0.py
@@ -0,0 +1,17 @@
+#a = 15
+#b = 14
+#c = 4
+#d = 2
+#e = a + b * c / d
+#print(e)
+
+#num = int(input("Enter a number from 1-100 here."))
+#print(num)
+
+#string = input("Enter one word here")
+#print(string)
+
+calc = int(input("Enter a number from 1-100 here."))
+calc2 = int(input("Enter a number from 1-100 here."))
+calc3 = calc + calc2
+print("You two numbers added together are ... ", calc3)
